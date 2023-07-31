b880eb2a80097336dd8706018faab86567dd7799
gogolevnd <gogolevnd@gmail.com>
2023-07-31 08:37:19 +0300

    gc hw;

---
+++ hw03_5-gc/src/main/java/ru/otus/calculator/Data.java
@@ -0,0 +1,16 @@
+package ru.otus.calculator;
+
+public class Data {
+    private int value;
+
+    public Data() {
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public int setValue(int value) {
+        this.value = value;
+        return value;
+    }
+}
